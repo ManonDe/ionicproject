7a60d8caff84658b7469c74428ed18cfc6d237e5
Manon <depreux.manon@gmail.com>
2017-10-26 17:07:41 +0200

    lirePhotos

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Camera } from '@ionic-native/camera';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 
 @Component({
   selector: 'page-camera',
@@ -11,7 +13,7 @@ export class CameraPage {
   app: any = {name: String, version:Number};
   img:String;
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(public navCtrl: NavController, private camera: Camera, private base64ToGallery: Base64ToGallery) {
     this.app.name = "AppName";
     this.app.version = 3.0;
 
@@ -27,6 +29,10 @@ export class CameraPage {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
       this.img = 'data:image/jpeg;base64,' + imageData;
+      this.base64ToGallery.base64ToGallery(base64Data, {prefix: '_img'}).then(
+        res => console.log('Saved image to gallery ', res),
+        err => console.log('Error saving image to gallery ', err)
+      );
     }, (err) => {
       // Handle error
     });
