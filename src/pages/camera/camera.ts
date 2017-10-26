272f0f5c85c0a59b5075732b803015c484955819
Manon <depreux.manon@gmail.com>
2017-10-26 16:44:14 +0200

    pageCamera

---
+++ src/pages/camera/camera.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-camera',
+  templateUrl: 'camera.html'
+})
+export class CameraPage {
+
+  app: any = {name: String, version:Number};
+  img:String;
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
+    this.app.name = "AppName";
+    this.app.version = 3.0;
+
+  }
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  };
+  runCamera() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class HomePage {
 
   app: any = {name: String, version:Number};
+  img:String;
 
   constructor(public navCtrl: NavController, private camera: Camera) {
     this.app.name = "AppName";
@@ -20,12 +21,12 @@ export class HomePage {
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
-    }
+    };
     runCamera() {
       this.camera.getPicture(this.options).then((imageData) => {
         // imageData is either a base64 encoded string or a file URI
         // If it's base64:
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        this.img = 'data:image/jpeg;base64,' + imageData;
       }, (err) => {
         // Handle error
       });
